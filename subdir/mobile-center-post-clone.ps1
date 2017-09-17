88952a7ea958fa3515f30e8aca67b4fbe8f8a392
Evgeniy Khramkov <v-evkhra@microsoft.com>
2017-09-17 22:01:02 +0300

    Update mobile-center-post-clone.ps1

--- subdir/mobile-center-post-clone.ps1
+++ subdir/mobile-center-post-clone.ps1
@@ -2,6 +2,4 @@ echo "============================================="
 echo "======= MOBILECENTER POSTCLONE SCRIPT ======="
 echo "============================================="
 
-if ("$env:username" -eq "var with 'sapces' `"and`" quotes") {
 Write-Host "variables $env:username $env:password"
-}
