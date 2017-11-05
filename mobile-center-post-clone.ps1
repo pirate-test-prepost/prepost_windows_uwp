c883863ab86fc7b66bd146d76c30854d2952dbaa
Evgeniy Khramkov <v-evkhra@microsoft.com>
2017-11-05 21:00:25 +0300

    Update mobile-center-post-clone.ps1

--- mobile-center-post-clone.ps1
+++ mobile-center-post-clone.ps1
@@ -4,3 +4,7 @@ echo "============================================="
 
 Write-Host "MOBILECENTER_BUILD_ID $MOBILECENTER_BUILD_ID"
 Write-Host "$username $password"
+
+if ($password -eq "123") {
+Write-Host "password valid"
+}
