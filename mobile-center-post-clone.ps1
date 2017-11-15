3f3469cd196b69679c0f45c339f72be8330136bf
Evgeniy Khramkov <v-evkhra@microsoft.com>
2017-11-15 08:45:06 +0300

    Update mobile-center-post-clone.ps1

--- mobile-center-post-clone.ps1
+++ mobile-center-post-clone.ps1
@@ -2,14 +2,8 @@ echo "============================================="
 echo "======= MOBILECENTER POSTCLONE SCRIPT ======="
 echo "============================================="
 
-Get-ChildItem Env:
-
-
-Write-Host "MOBILECENTER_BUILD_ID $MOBILECENTER_BUILD_ID"
-Write-Host "$env:username $env:password"
-
-
-
 if ($env:password -eq "123") {
 Write-Host "password valid"
+} else {
+Write-Host "password not valid"
 }
